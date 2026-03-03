c55e14053eb9b2462884be63996bdd20f49cdcb4
HENRY Hippolyte <hippolyte@bleu-robotics.com>
2026-03-03 15:44:51 +0100

    urdf_for_auto_collision

--- launch/manipulation_launcher.launch.py
+++ launch/manipulation_launcher.launch.py
@@ -19,6 +19,8 @@ def generate_launch_description():
     ik_max_dq_step = LaunchConfiguration("ik_max_dq_step")
     arm_velocity_limit = LaunchConfiguration("arm_velocity_limit")
     enable_collision_avoidance = LaunchConfiguration("enable_collision_avoidance")
+    send_cmds_to_robot = LaunchConfiguration("send_cmds_to_robot")
+    publish_joint_states_opensot = LaunchConfiguration("publish_joint_states_opensot")
 
     return LaunchDescription([
         DeclareLaunchArgument("interface", default_value=EnvironmentVariable("G1_INTERFACE")),
@@ -30,6 +32,8 @@ def generate_launch_description():
         DeclareLaunchArgument("ik_max_dq_step", default_value="0.05"),
         DeclareLaunchArgument("arm_velocity_limit", default_value="2.0"),
         DeclareLaunchArgument("enable_collision_avoidance", default_value="false"),
+        DeclareLaunchArgument("send_cmds_to_robot", default_value="true"),
+        DeclareLaunchArgument("publish_joint_states_opensot", default_value="false"),
 
         Node(
             package='g1pilot',
@@ -39,6 +43,8 @@ def generate_launch_description():
                 'interface': interface,
                 'use_robot': ParameterValue(use_robot, value_type=bool),
                 'enable_collision_avoidance': ParameterValue(enable_collision_avoidance, value_type=bool),
+                'send_cmds_to_robot': ParameterValue(send_cmds_to_robot, value_type=bool),
+                'publish_joint_states_opensot': ParameterValue(publish_joint_states_opensot, value_type=bool),
             }],
             output='screen'
         ),
